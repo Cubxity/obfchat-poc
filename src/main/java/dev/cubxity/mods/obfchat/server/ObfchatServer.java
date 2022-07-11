e6db9a85315dd8cbcb5626042d79a986cf78a985
Cubxity <contact@cubxity.dev>
2022-07-11 21:34:50 +0200

    chore: initial commit

---
+++ src/main/java/dev/cubxity/mods/obfchat/server/ObfchatServer.java
@@ -0,0 +1,68 @@
+package dev.cubxity.mods.obfchat.server;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.fabric.api.message.v1.ServerMessageDecoratorEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.contents.LiteralContents;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
+
+public class ObfchatServer implements DedicatedServerModInitializer {
+    // This is the destination "unicode page".
+    // The page number (represented in hex) can be found in the file name:
+    // For example: "unicode_page_4c.png"
+    private static final int START_CODE_POINT = 0x4c * 256;
+
+    // 2607 (Hex) 9735
+    // 0061 (hex) 97
+
+    private final Gson gson = new Gson();
+
+    @Override
+    public void onInitializeServer() {
+        try {
+            var json = Files.readString(Path.of("glyphs.json"));
+            var array = gson.fromJson(json, JsonArray.class);
+            var map = new int[256];
+            for (int i = 0; i < 255; i++) {
+                map[i] = array.get(i).getAsInt() + START_CODE_POINT;
+            }
+
+            ServerMessageDecoratorEvent.EVENT.register((serverPlayer, component) ->
+                CompletableFuture.completedFuture(modify(component, map)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private MutableComponent modify(Component component, int[] map) {
+        var contents = component.getContents();
+        if (contents instanceof LiteralContents literal) {
+            var builder = new StringBuilder();
+            literal.text().codePoints().forEach(codePoint -> {
+                if (codePoint > 255) {
+                    builder.appendCodePoint(codePoint);
+                } else {
+                    builder.appendCodePoint(map[codePoint]);
+                }
+            });
+            contents = new LiteralContents(builder.toString());
+        }
+
+        MutableComponent newComponent = MutableComponent.create(contents);
+        newComponent.setStyle(component.getStyle());
+
+
+        for (Component sibling : component.getSiblings()) {
+            newComponent.append(modify(sibling, map));
+        }
+
+        return newComponent;
+    }
+}
---
+++ src/test/java/dev/cubxity/mods/obfchat/gen/ObfchatGenerator.java
@@ -0,0 +1,83 @@
+package dev.cubxity.mods.obfchat.gen;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+public class ObfchatGenerator {
+    private static final int START_CODE_POINT = 0x4c * 256;
+
+    public static void main(String[] args) throws IOException {
+        var source = ImageIO.read(ObfchatGenerator.class.getResourceAsStream("/font/ascii.png"));
+        var dest = new BufferedImage(128, 128, BufferedImage.TYPE_INT_ARGB);
+        var graphics = dest.getGraphics();
+
+        // Shuffle the glyphs
+        var map = new ArrayList<>(IntStream.range(0, 256).boxed().toList());
+//        Collections.shuffle(map);
+
+        // Map the glyphs to random places as defined above
+        for (var sourceIndex = 0; sourceIndex < 256; sourceIndex++) {
+            var sourceX = (sourceIndex % 16) * 8;
+            var sourceY = (Math.floorDiv(sourceIndex, 16)) * 8;
+
+            var destIndex = map.get(sourceIndex);
+            var destX = (destIndex % 16) * 8;
+            var destY = (Math.floorDiv(destIndex, 16)) * 8;
+
+            var data = source.getSubimage(sourceX, sourceY, 8, 8);
+            graphics.drawImage(data, destX, destY, null);
+        }
+
+        // Export glyphs and data
+        var fontTextureDir = new File("run/resourcepacks/glyphs/assets/minecraft/textures/font/");
+        fontTextureDir.mkdirs();
+        ImageIO.write(dest, "PNG", new File(fontTextureDir, "glyphs.png"));
+
+        var fontProvider = new JsonObject();
+        fontProvider.addProperty("type", "bitmap");
+        fontProvider.addProperty("file", "minecraft:font/glyphs.png");
+        fontProvider.addProperty("ascent", 7);
+
+        var fontProviderChars = new JsonArray();
+        for (var row = 0; row < 16; row++) {
+            var chars = new StringBuilder();
+            for (var col = 0; col < 16; col++) {
+                var index = row * 16 + col;
+                chars.append(String.format("\\u%04X", START_CODE_POINT + map.get(index)));
+            }
+            fontProviderChars.add(chars.toString());
+        }
+        fontProvider.add("chars", fontProviderChars);
+
+        var fontProviders = new JsonArray();
+        fontProviders.add(fontProvider);
+
+        var fontJson = new JsonObject();
+        fontJson.add("providers", fontProviders);
+
+        var fontDir = Path.of("run/resourcepacks/glyphs/assets/minecraft/font/");
+        Files.createDirectories(fontDir);
+
+        // FIXME: is there a better way to do this?
+        Files.writeString(fontDir.resolve("default.json"), fontJson.toString().replace("\\\\", "\\"));
+
+
+        var glyphsMap = new JsonArray();
+        for (int i : map) {
+            glyphsMap.add(i);
+        }
+
+        var runDir = Path.of("run");
+        Files.createDirectories(runDir);
+        Files.writeString(runDir.resolve("glyphs.json"), glyphsMap.toString());
+    }
+}
